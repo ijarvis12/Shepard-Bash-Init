0e01e88ab7527c2b2c771bac272f80031f7e2c98
Ian Jarvis <ijarvis12@gmail.com>
2024-08-31 06:39:36 -0500

    Update halt.sh

--- halt.sh
+++ halt.sh
@@ -14,3 +14,15 @@ PS="$(ps fax | awk '{print $1}')"
 for ps in "$PS"; do
   kill $ps
 done
+
+for dev in "$(ls -1 /media)"; do
+  umount "$dev"
+done
+
+for dev in "$(ls -1 /mnt)"; do
+  umount "$dev"
+done
+
+umount "/boot"
+
+umount "/"
