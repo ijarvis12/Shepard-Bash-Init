825cd434ac3a2aa001ed4d0449eee4489f1a016d
Ian Jarvis <ijarvis12@gmail.com>
2024-08-31 06:35:23 -0500

    Update halt.sh

--- halt.sh
+++ halt.sh
@@ -1,3 +1,16 @@
 #!/usr/bin/env bash
 
 kill -s SIGPWR 1
+
+PS="$(ps fax | awk '{print $1}')"
+
+for ps in "$PS"; do
+  kill -s SIGSTOP $ps
+done
+
+sleep 8
+PS="$(ps fax | awk '{print $1}')"
+
+for ps in "$PS"; do
+  kill $ps
+done
