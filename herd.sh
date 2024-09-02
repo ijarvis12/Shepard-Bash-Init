932883c766a1ec126990c25e03d27389b947f3b4
Ian Jarvis <ijarvis12@gmail.com>
2024-09-02 14:04:22 -0500

    Update herd.sh

--- herd.sh
+++ herd.sh
@@ -4,18 +4,18 @@ function start() {
   echo "starting $1"
   ("/etc/shepard/$1" & && \
   sleep 2 && \
-  PID=$(ps ax | grep "$1" | grep -v grep | head -1 | awk '{print $1}') && \
-  echo "$1 started with PID of $PID" && \
-  echo "started $1 ["$(tput setaf 2)'OK'$(tput sgr0)"]") || \
-  echo "started $1 ["$(tput setaf 1)'NOT OK'$(tput sgr0)"]"
+  pid=$(ps ax | grep "$1" | grep -v grep | head -1 | awk '{print $1}') && \
+  echo "$1 started with PID of $pid" && \
+  echo "started $1 [ "$(tput setaf 2)'OK'$(tput sgr0)" ]") || \
+  echo "started $1 [ "$(tput setaf 1)'NOT OK'$(tput sgr0)" ]"
 }
 
 function stop() {
   echo "stopping $1"
-  PID=$(ps ax | grep "$1" | grep -v grep | head -1 | awk '{print $1}')
-  (kill "$PID" && \
-  echo "$1 stopped ["$(tput setaf 2)'OK'$(tput sgr0)"]") || \
-  echo "$1 stopped ["$(tput setaf 1)'NOT OK'$(tput sgr0)"]"
+  pid=$(ps ax | grep "$1" | grep -v grep | head -1 | awk '{print $1}')
+  (kill "$pid" && \
+  echo "$1 stopped [ "$(tput setaf 2)'OK'$(tput sgr0)" ]") || \
+  echo "$1 stopped [ "$(tput setaf 1)'NOT OK'$(tput sgr0)" ]"
 }
 
 function reload() {
